434cf75acb05a7c5d89694e57abd2014544b6949
Kevin Vervier <kevin.vervier04@gmail.com>
2018-03-01 20:23:51 -0600

    jsonAjax

---
+++ google_script_form_ajax.js
@@ -0,0 +1,14 @@
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycby7IJigwmpzMnw8801h9KKSdUrwMbKSdwfvnKNVNM_KbcpT9UVR/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
